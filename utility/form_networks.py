e391993afd836d4a99cb2d9d25a125a137018e29
k-z-m-r <jdkazime@buffalo.edu>
2021-06-01 23:35:50 -0400

    added one_hot and adjacency matrix
    all the utility functions are done, so we can begin analysis if we want

--- utility/form_networks.py
+++ utility/form_networks.py
@@ -32,3 +32,46 @@ def get_voting_array(df, drop = True):
         V = np.delete(V, to_drop, axis = 0)
         
     return V
+
+def get_one_hot(votes_df, ideology_df):
+    
+    '''
+        votes_df -> the dataframe encoding votes that is used to get V.
+        ideology_df -> this stores party information.
+    '''
+    
+    ids = ideology_df['icpsr']
+    
+    p_ids = np.zeros((ids.shape[0]))
+
+    for idx, c_id in enumerate(np.unique(votes_df['icpsr'])):
+        p_ids[idx] = ideology_df[c_id == ids]['party_code']
+        
+    unq_parties = np.unique(p_ids)
+    
+    one_hot = np.zeros((p_ids.shape[0], unq_parties.shape[0]))
+    
+    for idx, p in enumerate(unq_parties):
+        
+        one_hot[:, idx] = (p == p_ids).astype(int)
+    
+    return one_hot, p_ids
+
+def get_adjacency(V):
+    
+    '''
+        V -> the array containing voting trends.
+    '''
+    
+    N = V.shape[0]
+    A = np.zeros((N, N))
+    
+    for idx, v_0 in enumerate(V):
+        for jdx, v_1 in enumerate(V):
+
+            if idx != jdx:
+                locs = v_0 == v_1
+                sim = np.sum(locs)/locs.shape[0]
+                A[idx, jdx] = sim
+                
+    return A
